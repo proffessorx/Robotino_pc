c5196a3f62410a096e61a98ecb6ffd5bd6607cc2
proffessorx <ali_haider7862002@yahoo.com>
2018-04-18 15:06:22 +0200

    added Hector_mapping

---
+++ devel/share/hector_nav_msgs/cmake/hector_nav_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(hector_nav_msgs_MESSAGE_FILES "")
+set(hector_nav_msgs_SERVICE_FILES "/home/robotino/catkin_ws/src/hector_slam/hector_nav_msgs/srv/GetDistanceToObstacle.srv;/home/robotino/catkin_ws/src/hector_slam/hector_nav_msgs/srv/GetRecoveryInfo.srv;/home/robotino/catkin_ws/src/hector_slam/hector_nav_msgs/srv/GetRobotTrajectory.srv;/home/robotino/catkin_ws/src/hector_slam/hector_nav_msgs/srv/GetSearchPosition.srv;/home/robotino/catkin_ws/src/hector_slam/hector_nav_msgs/srv/GetNormal.srv")
